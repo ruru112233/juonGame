d07aa799a143ee8a5c6e195d80d016681af26f7c
ruru112233 <esxwaz123@hotmail.co.jp>
2024-06-07 21:11:52 +0900

    TimeManagerの追加

---
+++ Assets/Scripts/GameScene/TimeManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeManager : MonoBehaviour
+{
+    const string TIME_MSG = "Time:";
+
+    public TextMeshProUGUI timerText;
+
+    private float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.text = TIME_MSG + timer.ToString("F1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.text = TIME_MSG + timer.ToString("F1");
+    }
+}
