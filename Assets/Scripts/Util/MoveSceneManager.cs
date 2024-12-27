8399bd6f9a076e2ac3049b2eb59fe649e90a7652
ruru112233 <esxwaz123@hotmail.co.jp>
2024-12-27 22:15:24 +0900

    ランキングパネル非表示対応

--- Assets/Scripts/Util/MoveSceneManager.cs
+++ Assets/Scripts/Util/MoveSceneManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MoveSceneManager : MonoBehaviour
 {
@@ -18,6 +20,52 @@ public class MoveSceneManager : MonoBehaviour
     public void EndingSceneButton(float time)
     {
         StartCoroutine(MoveScene(time, "GameCrearScene"));
+    }
+
+    public void Rankinghidden()
+    {
+        GameObject rankingPanel = GameObject.FindGameObjectWithTag("RankingPanel");
+
+        if (rankingPanel)
+        {
+            RetryButtonHidden();
+            EndingButtonHidden();
+            rankingPanel.SetActive(false);
+        }
+    }
+
+    private void RetryButtonHidden()
+    {
+        GameObject retryButton = GameObject.FindGameObjectWithTag("RetryButton");
+
+        if (retryButton)
+        {
+            Button button = retryButton.GetComponent<Button>();
+            button.interactable = false;
+
+            Image buttnImage = retryButton.GetComponent<Image>();
+            buttnImage.color = new Color(0, 0, 0, 0);
+
+            TextMeshProUGUI text = retryButton.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            text.color = new Color(0, 0, 0, 0);
+        }
+    }
+
+    private void EndingButtonHidden()
+    {
+        GameObject endingButton = GameObject.FindGameObjectWithTag("EndingButton");
+
+        if (endingButton)
+        {
+            Button button = endingButton.GetComponent<Button>();
+            button.interactable = false;
+
+            Image buttnImage = endingButton.GetComponent<Image>();
+            buttnImage.color = new Color(0, 0, 0, 0);
+
+            TextMeshProUGUI text = endingButton.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            text.color = new Color(0, 0, 0, 0);
+        }
     }
 
     IEnumerator MoveScene(float time, string sceneName)
