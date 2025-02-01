ea5f14cc5207343ede0c6b182b9342a1c2f57bf2
ruru112233 <esxwaz123@hotmail.co.jp>
2025-02-01 17:40:07 +0900

    音量調整機能追加

--- Assets/Scripts/TitleScene/OptionManager.cs
+++ Assets/Scripts/TitleScene/OptionManager.cs
@@ -1,19 +1,33 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour
 {
     private const int MAX_MSG_SPEED = 3;
     private const int MIN_MSG_SPEED = 1;
+    private const float MAX_VOLUME = 0.9f;
+    private const float MIN_VOLUME = 0.1f;
+    private const float VOLUME_DISTANCE = 0.2f;
 
     [SerializeField] TextMeshProUGUI msgSpd1, msgSpd2, msgSpd3, msgSpd4, msgSpd5, msgSpd6;
+
+    [SerializeField] private Scrollbar bgmVolumeScrollbar, seVolumeScrollbar;
 
     Color colorInit = new Color32(147, 147, 147, 255);
     Color decisionColor = new Color32(255, 255, 255, 255);
 
     private int msgSpeedSelectNum = 0;
+    private float bgmVolumeNum = 0.6f;
+    private float seVolumeNum = 0.6f;
+
+    struct VolumeData
+    {
+        public float bgmVolume;
+        public float seVolume;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +37,14 @@ public class OptionManager : MonoBehaviour
         SettingSaveData data = SaveAndLoader.Load<SettingSaveData>();
 
         msgSpeedSelectNum = data.msgSpeed;
+        bgmVolumeNum = data.bgmVolume;
+        seVolumeNum = data.seVolume;
+
+        bgmVolumeScrollbar.value = bgmVolumeNum;
+        seVolumeScrollbar.value = seVolumeNum;
+
+        AudioManager.instance.BgmSliderVolume(bgmVolumeNum);
+        AudioManager.instance.SeSliderVolume(seVolumeNum);
 
         SettingMsgSpeed(data.msgSpeed);
     }
@@ -62,11 +84,78 @@ public class OptionManager : MonoBehaviour
         SettingMsgSpeed(msgSpeedSelectNum);
     }
 
+    public void LeftBgmSelectButton()
+    {
+        if (MIN_VOLUME >= bgmVolumeNum) return;
+
+        bgmVolumeNum -= VOLUME_DISTANCE;
+
+        VolumeSave();
+    }
+
+    public void RightBgmSelectButton()
+    {
+        if (MAX_VOLUME <= bgmVolumeNum) return;
+
+        bgmVolumeNum += VOLUME_DISTANCE;
+
+        VolumeSave();
+    }
+
+    public void LeftSeSelectButton()
+    {
+        if (MIN_VOLUME >= seVolumeNum) return;
+
+        seVolumeNum -= VOLUME_DISTANCE;
+
+        VolumeSave();
+    }
+
+    public void RightSeSelectButton()
+    {
+        if (MAX_VOLUME <= seVolumeNum) return;
+
+        seVolumeNum += VOLUME_DISTANCE;
+
+        VolumeSave();
+    }
+
+    void SetVolumeScrollBar(VolumeData volumeData)
+    {
+        bgmVolumeScrollbar.value = volumeData.bgmVolume;
+        seVolumeScrollbar.value = volumeData.seVolume;
+
+        AudioManager.instance.BgmSliderVolume(volumeData.bgmVolume);
+        AudioManager.instance.SeSliderVolume(volumeData.seVolume);
+    }
+
+    void VolumeSave()
+    {
+        VolumeData volumeData = new VolumeData();
+
+        volumeData.bgmVolume = bgmVolumeNum;
+        volumeData.seVolume = seVolumeNum;
+
+        OptionSave(volumeData);
+    }
+
     void OptionSave(EnumData.MsgSpeedType msgSpeedType)
     {
         SettingSaveData data = SaveAndLoader.Load<SettingSaveData>();
 
         data.msgSpeed = (int)msgSpeedType;
+
+        SaveAndLoader.Save(data);
+    }
+
+    void OptionSave(VolumeData volumeData)
+    {
+        SettingSaveData data = SaveAndLoader.Load<SettingSaveData>();
+
+        data.bgmVolume = volumeData.bgmVolume;
+        data.seVolume = volumeData.seVolume;
+
+        SetVolumeScrollBar(volumeData);
 
         SaveAndLoader.Save(data);
     }
